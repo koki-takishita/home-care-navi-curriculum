448b050d2d891c113db0c0bda2af2e7fcdae9dc2
koki-takishita <tkkokitk@yahoo.co.jp>
2022-11-12 17:20:25 +0900

    install vuetify

--- front/plugins/vuetify.ts
+++ front/plugins/vuetify.ts
@@ -1,9 +1,20 @@
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import { aliases, mdi } from 'vuetify/lib/iconsets/mdi'
+
+// materialdesign iconのfont これないとiconが表示されない
+import '@mdi/font/css/materialdesignicons.css'
 
 export default defineNuxtPlugin(nuxtApp => {
   const vuetify = createVuetify({
+    icons: {
+      defaultSet: 'mdi',
+      aliases,
+      sets: {
+        mdi,
+      }
+    },
     components,
     directives,
   })
